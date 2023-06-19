197ac83d2b8105cddcccad6c700462f7b19e21cb
DARKEST-OWL-HACKING-SOCIETY <129428958+DARKEST-OWL-HACKING-SOCIETY@users.noreply.github.com>
2023-06-19 12:00:29 +0100

    Create run.py

---
+++ run.py
@@ -0,0 +1,14 @@
+import os, sys
+
+try:os.system('xdg-open https://chat.whatsapp.com/LQGUIDUWK5gFVjJjreQ0vp')
+
+except:pass
+
+try:
+
+    __import__("soize").Instagram()
+
+except Exception as e:
+
+    exit(str(e))
+
