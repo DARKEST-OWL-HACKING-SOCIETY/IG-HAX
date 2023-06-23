daf79d0cc01fee41d835876174eae8018963db09
DARKEST-OWL-HACKING-SOCIETY <129428958+DARKEST-OWL-HACKING-SOCIETY@users.noreply.github.com>
2023-06-23 19:59:39 +0100

    Create Hax.py

---
+++ Hax.py
@@ -0,0 +1,10 @@
+import os
+import platform
+os.system('clear')
+os.system("git pull")
+b = platform.architecture()[0]
+if b == '64bit':
+    __import__("soize").Instagram()
+    
+elif b == '32bit':
+    print(" TYPE pytho run.py to switch to 32bit")
